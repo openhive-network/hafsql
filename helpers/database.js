df9e5d42c0266b3ff41062e775dc1df910469d4e
Mahdi Yari <m.yary15@gmail.com>
2023-05-07 02:06:09 +0330

    Update database.js

--- helpers/database.js
+++ helpers/database.js
@@ -9,6 +9,7 @@ export const pool = new pg.Pool({
   host: process.env.PGHOST || '172.17.0.2',
   port: process.env.PGPORT || 5432,
   max: process.env.PGPOOLSIZE || 10,
-  min: 1
+  min: 1,
+  ssl: true
 })
 
