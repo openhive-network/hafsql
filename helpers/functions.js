583fd21cf614d35c3746a5236272c513ff6844fe
Mahdi Yari <m.yary15@gmail.com>
2023-05-08 21:07:11 +0330

    Create functions.js

---
+++ helpers/functions.js
@@ -0,0 +1,17 @@
+import { pool } from './database.js'
+
+export const setupFunctions = async () => {
+  const AssetAmount = `CREATE OR REPLACE FUNCTION hafsql_assetamount(text) RETURNS float
+    AS $$ SELECT ($1::jsonb->>'amount')::int / power(10, ($1::jsonb->>'precision')::int) $$
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`
+  await pool.query(AssetAmount)
+
+  const AssetSymbol = `CREATE OR REPLACE FUNCTION hafsql_assetsymbol(text) RETURNS text
+    AS $$ SELECT CASE WHEN $1::jsonb->>'nai' = '@@000000013' THEN 'HBD' WHEN $1::jsonb->>'nai' = '@@000000037' THEN 'VESTS' ELSE 'HIVE' END $$
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`
+  await pool.query(AssetSymbol)
+}
