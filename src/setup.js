d7c3858939ada054dd98235a40032d38871006ba
Mahdi Yari <m.yary15@gmail.com>
2023-05-08 21:07:00 +0330

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -1,11 +1,13 @@
 import { pool } from '../helpers/database.js'
-import { removeOperationViews, setupOperationViews } from './operations.js'
-import { removeVirtualOperationViews, setupVirtualOperationViews } from './virtualOperations.js'
+import { removeOperationViews, setupOperationViews } from '../helpers/operations.js'
+import { removeVirtualOperationViews, setupVirtualOperationViews } from '../helpers/virtualOperations.js'
+import { setupFunctions } from '../helpers/functions.js'
+import { setupSchema } from '../helpers/schema.js'
 
 // Creating hafsql schema and all the views
 export const setup = async () => {
-  const schema = `CREATE SCHEMA IF NOT EXISTS hafsql;`
-  await pool.query(schema)
+  console.log('Setting up the schema...')
+  await setupSchema()
 
   console.log('Creating the functions...')
   await setupFunctions()
@@ -21,28 +23,8 @@ export const setup = async () => {
   // console.log('Creating indexes...')
   // await setupOperationIndexes()
   // await setupVirtualOperationIndexes()
-
+  await pool.end()
   console.log('Everything ready.')
-}
-
-export const createIndexes = async () => {
-  // 
-}
-
-const setupFunctions = async () => {
-  const AssetAmount = `CREATE OR REPLACE FUNCTION hafsql_assetamount(text) RETURNS float
-    AS $$ SELECT ($1::jsonb->>'amount')::int / power(10, ($1::jsonb->>'precision')::int) $$
-    LANGUAGE SQL
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await pool.query(AssetAmount)
-
-  const AssetSymbol = `CREATE OR REPLACE FUNCTION hafsql_assetsymbol(text) RETURNS text
-    AS $$ SELECT CASE WHEN $1::jsonb->>'nai' = '@@000000013' THEN 'HBD' WHEN $1::jsonb->>'nai' = '@@000000037' THEN 'VESTS' ELSE 'HIVE' END $$
-    LANGUAGE SQL
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await pool.query(AssetSymbol)
 }
 
 setup()
