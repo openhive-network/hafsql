9ec7f1ec59bae09a59b13b3e508a4e26f41bb299
Mahdi Yari <m.yary15@gmail.com>
2023-05-14 19:26:00 +0330

    Update rcDelegations.js

--- helpers/syncs/rcDelegations.js
+++ helpers/syncs/rcDelegations.js
@@ -75,7 +75,7 @@ const insertRCDelegations = async ( delegation ) => {
     }
     await pool.query(`INSERT INTO hafsql.rc_delegations_table (delegator, delegatee, rc)
       VALUES ($1, $2, $3) ON CONFLICT ON CONSTRAINT hafsql_rc_delegations_table_un
-      DO UPDATE SET rc=$3;`, [from, delegatee, rc])
+      DO UPDATE SET rc=$3;`, [from, delegatee, maxRC])
   }
   return true
 }
