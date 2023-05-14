982df94c70c99ece7ce5e9392d1cb5e263ad3107
Mahdi Yari <m.yary15@gmail.com>
2023-05-14 19:11:25 +0330

    Update rcDelegations.js

--- helpers/syncs/rcDelegations.js
+++ helpers/syncs/rcDelegations.js
@@ -26,7 +26,7 @@ export const fillRCDelegations = async (limit = 20000) => {
 }
 // 	"[0,{"from":"mahdiyari","delegatees":["gtg"],"max_rc":1889000000, "test": 11}]"
 const getRCDelegations = async (start, limit = 10000) => {
-  const result = await pool.query(`SELECT op_id, json, required_posting_auths FROM hafsql."TxCustomJson"
+  const result = await pool.query(`SELECT op_id, json FROM hafsql."TxCustomJson"
     WHERE id=$1 AND op_id > $2 ORDER BY op_id ASC LIMIT $3`, ['rc', start, limit])
   console.log(result.rowCount, result.rows[0])
   if (result.rowCount <= 0) {
@@ -38,6 +38,7 @@ const getRCDelegations = async (start, limit = 10000) => {
     const rcDelegation = result.rows[i]
     try {
       const parsedJson = JSON.parse(rcDelegation.json)
+      console.log(parsedJson)
       if (!Array.isArray(parsedJson)) {
         continue
       }
