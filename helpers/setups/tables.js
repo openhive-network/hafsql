e52108ef6ae227ecef55c53075d47b711ed4ef21
Mahdi Yari <m.yary15@gmail.com>
2023-05-15 16:28:07 +0330

    Update tables.js

--- helpers/setups/tables.js
+++ helpers/setups/tables.js
@@ -1,4 +1,4 @@
-import { pool } from "../database.js"
+import { pool } from '../database.js'
 
 export const setupTables = async () => {
   // Sync data
@@ -16,7 +16,9 @@ export const setupTables = async () => {
     vests varchar NOT NULL,
     CONSTRAINT hafsql_delegations_table_un UNIQUE (delegator, delegatee)
   );`)
-  await pool.query(`CREATE INDEX IF NOT EXISTS hafsql_delegations_table_delegatee_idx ON hafsql.delegations_table USING btree (delegatee);`)
+  await pool.query(
+    'CREATE INDEX IF NOT EXISTS hafsql_delegations_table_delegatee_idx ON hafsql.delegations_table USING btree (delegatee);'
+  )
 
   // RC Delegations
   await pool.query(`CREATE TABLE IF NOT EXISTS hafsql.rc_delegations_table (
@@ -25,19 +27,31 @@ export const setupTables = async () => {
     rc varchar NOT NULL,
     CONSTRAINT hafsql_rc_delegations_table_un UNIQUE (delegator, delegatee)
   );`)
-  await pool.query(`CREATE INDEX IF NOT EXISTS hafsql_rc_delegations_table_delegatee_idx ON hafsql.rc_delegations_table USING btree (delegatee);`)
+  await pool.query(
+    'CREATE INDEX IF NOT EXISTS hafsql_rc_delegations_table_delegatee_idx ON hafsql.rc_delegations_table USING btree (delegatee);'
+  )
+
+  // Proposal Approvals
+  await pool.query(`CREATE TABLE hafsql.proposal_approvals_table (
+    id int4 NOT NULL,
+    voter varchar(16) NOT NULL,
+    CONSTRAINT hafsql_proposal_approvals_table_un UNIQUE (id, voter)
+  );`)
 }
 
 const setupSyncDataTable = async () => {
-  const tableNames = [
-    'delegations',
-    'rc_delegations'
-  ]
+  const tableNames = ['delegations', 'rc_delegations', 'proposal_approvals']
   for (let i = 0; i < tableNames.length; i++) {
     const name = tableNames[i]
-    const data = await pool.query('SELECT * FROM hafsql.sync_data WHERE table_name = $1', [name])
+    const data = await pool.query(
+      'SELECT last_op_id FROM hafsql.sync_data WHERE table_name = $1',
+      [name]
+    )
     if (!data.rowCount) {
-      await pool.query('INSERT INTO hafsql.sync_data(table_name, last_op_id) VALUES($1, $2)', [name, 0])
+      await pool.query(
+        'INSERT INTO hafsql.sync_data(table_name, last_op_id) VALUES($1, $2)',
+        [name, 0]
+      )
     }
   }
 }
