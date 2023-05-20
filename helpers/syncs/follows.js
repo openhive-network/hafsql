bcaa93f64e6e207d3a796d02c648524a929853f5
Mahdi Yari <m.yary15@gmail.com>
2023-05-20 19:08:29 +0330

    Update follows.js

--- helpers/syncs/follows.js
+++ helpers/syncs/follows.js
@@ -4,6 +4,8 @@ import { validateAccountName } from '../validateUsername.js'
 let accountCache = {}
 let useCache = true
 
+let followersArray = []
+
 export const syncFollows = async () => {
   useCache = false
   accountCache = {}
@@ -27,19 +29,14 @@ export const fillFollows = async (limit = 20000) => {
   )
   start = start.rows[0].last_op_id
   let follows = await getFollows(start, limit)
-  let i = 0
   while (follows.length > 0) {
-    await insertFollows(follows[i])
-    i++
-    if (i >= follows.length) {
-      i = 0
-      const start = follows[follows.length - 1].op_id
-      await updateLastOpId(start)
-      // if (start >= 100000000) {
-      //   break
-      // }
-      follows = await getFollows(start, limit)
+    await insertFollows(follows)
+    const start = follows[follows.length - 1].op_id
+    await updateLastOpId(start)
+    if (start >= 100000000) {
+      break
     }
+    follows = await getFollows(start, limit)
   }
 }
 
@@ -87,8 +84,12 @@ const getFollows = async (start, limit = 10000) => {
         ) {
           continue
         }
-        const { follower, following, what } = parsedJson[1]
-        if (validateAccountName(follower) || validateAccountName(following)) {
+        const { follower, what } = parsedJson[1]
+        let { following } = parsedJson[1]
+        if (!Array.isArray(following)) {
+          following = [following]
+        }
+        if (validateAccountName(follower)) {
           continue
         }
         if (postingAuths[0] !== follower) {
@@ -97,19 +98,11 @@ const getFollows = async (start, limit = 10000) => {
         if (!Array.isArray(what) || what.length > 1) {
           continue
         }
-        const ids = {}
-        ids[follower] = await getUserId(follower)
-        if (!ids[follower]) {
-          continue
-        }
-        ids[following] = await getUserId(following)
-        if (!ids[following]) {
-          continue
-        }
+        const ids = await processFollowing(follower, following, what)
         followsArray.push({
           type,
-          follower: ids[follower],
-          following: ids[following],
+          follower: ids.follower,
+          following: ids.following,
           what,
           op_id: customJson.op_id
         })
@@ -151,6 +144,27 @@ const getFollows = async (start, limit = 10000) => {
   return followsArray
 }
 
+const processFollowing = async (follower, following, what) => {
+  const followingsArray = []
+  const ids = {}
+  ids.follower = await getUserId(follower)
+  for (let i = 0; i < following.length; i++) {
+    if (validateAccountName(following[i])) {
+      continue
+    }
+    if (follower === following[i]) {
+      continue
+    }
+    const followingId = await getUserId(following[i])
+    if (!followingId) {
+      continue
+    }
+    followingsArray.push(followingId)
+  }
+  ids.following = followingsArray
+  return ids
+}
+
 // Caching ids for duration of the sync
 const getUserId = async (username) => {
   if (useCache && Object.hasOwn(accountCache, username)) {
@@ -166,143 +180,203 @@ const getUserId = async (username) => {
   }
 }
 
-// defs = {
-//   '': Action.Nothing,
-//   'blog': Action.Blog,
-//   'follow': Action.Blog,
-//   'ignore': Action.Ignore,
-//   'blacklist': Action.Blacklist,
-//   'follow_blacklist': Action.Follow_blacklist,
-//   'unblacklist': Action.Unblacklist,
-//   'unfollow_blacklist': Action.Unfollow_blacklist,
-//   'follow_muted': Action.Follow_muted,
-//   'unfollow_muted': Action.Unfollow_muted,
-//   'reset_blacklist': Action.Reset_blacklist,
-//   'reset_following_list': Action.Reset_following_list,
-//   'reset_muted_list': Action.Reset_muted_list,
-//   'reset_follow_blacklist': Action.Reset_follow_blacklist,
-//   'reset_follow_muted_list': Action.Reset_follow_muted_list,
-//   'reset_all_lists': Action.Reset_all_lists,
-// }
+const followersHelper = (item, action) => {
+  const { follower, following } = item
+  for (let i = 0; i < following.length; i++) {
+    if (action === 'follow') {
+      followersArray.push({ follower, following: following[i] })
+    } else {
+      for (let k = 0; k < followersArray.length; k++) {
+        const temp = followersArray[k]
+        if (typeof temp === 'undefined') {
+          continue
+        }
+        if (temp.follower === follower && temp.following === following[i]) {
+          delete followersArray[k]
+        }
+      }
+    }
+  }
+}
 
 const insertFollows = async (follow) => {
-  if (follow.type === 'reblog') {
-    return insertReblog(follow)
+  followersArray = []
+  for (let i = 0; i < follow.length; i++) {
+    const item = follow[i]
+    if (item.type === 'reblog') {
+      await insertReblog(item)
+      continue
+    }
+    if (item.what.length === 0) {
+      followersHelper(item, 'unfollow')
+      await unfollowUnmute(follow)
+      continue
+    }
+    const what = item.what[0]
+    switch (what) {
+      case 'blacklist':
+        await blacklist(item)
+        break
+      case 'unblacklist':
+        await unblacklist(item)
+        break
+      case 'follow_blacklist':
+        await followBlacklist(item)
+        break
+      case 'unfollow_blacklist':
+        await unfollowBlacklist(item)
+        break
+      case 'follow_muted':
+        await followMuted(item)
+        break
+      case 'unfollow_muted':
+        await unfollowMuted(item)
+        break
+      case 'follow':
+      case 'blog':
+        followersHelper(item, 'follow')
+        break
+      case 'ignore':
+        await mute(item)
+        break
+      case 'reset_blacklist':
+        await resetBlacklist(item)
+        break
+      case 'reset_following_list':
+        await resetFollowingList(item)
+        break
+      case 'reset_muted_list':
+        await resetMutedList(item)
+        break
+      case 'reset_follow_blacklist':
+        await resetFollowBlacklist(item)
+        break
+      case 'reset_follow_muted_list':
+        await resetFollowMutedList(item)
+        break
+      case 'reset_all_lists':
+        await resetAllLists(item)
+        break
+      default:
+        break
+    }
   }
-  if (follow.what.length === 0) {
-    return unfollowUnmute(follow)
-  }
-  const what = follow.what[0]
-  switch (what) {
-    case 'blacklist':
-      return blacklist(follow)
-    case 'unblacklist':
-      return unblacklist(follow)
-    case 'follow_blacklist':
-      return followBlacklist(follow)
-    case 'unfollow_blacklist':
-      return unfollowBlacklist(follow)
-    case 'follow_muted':
-      return followMuted(follow)
-    case 'unfollow_muted':
-      return unfollowMuted(follow)
-    case 'follow':
-    case 'blog':
-      return actualFollow(follow)
-    case 'ignore':
-      return mute(follow)
-    case 'reset_blacklist':
-      return resetBlacklist(follow)
-    case 'reset_following_list':
-      return resetFollowingList(follow)
-    case 'reset_muted_list':
-      return resetMutedList(follow)
-    case 'reset_follow_blacklist':
-      return resetFollowBlacklist(follow)
-    case 'reset_follow_muted_list':
-      return resetFollowMutedList(follow)
-    case 'reset_all_lists':
-      return resetAllLists(follow)
-    default:
-      break
-  }
+  await actualFollow()
 }
 
-const blacklist = async (follow) => {
-  await pool.query(
-    `INSERT INTO hafsql.blacklists_table (blacklister, blacklisted)
-      VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_blacklists_table_un
-      DO NOTHING;`,
-    [follow.follower, follow.following]
-  )
+const blacklist = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `INSERT INTO hafsql.blacklists_table (blacklister, blacklisted)
+        VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_blacklists_table_un
+        DO NOTHING;`,
+      [follower, following[i]]
+    )
+  }
 }
-const unblacklist = async (follow) => {
-  await pool.query(
-    `DELETE FROM hafsql.blacklists_table
-      WHERE blacklister=$1 AND blacklisted=$2;`,
-    [follow.follower, follow.following]
-  )
+const unblacklist = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `DELETE FROM hafsql.blacklists_table
+        WHERE blacklister=$1 AND blacklisted=$2;`,
+      [follower, following[i]]
+    )
+  }
 }
-const followBlacklist = async (follow) => {
-  await pool.query(
-    `INSERT INTO hafsql.blacklist_follows_table (account, blacklist)
-      VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_blacklist_follows_table_un
-      DO NOTHING;`,
-    [follow.follower, follow.following]
-  )
+const followBlacklist = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `INSERT INTO hafsql.blacklist_follows_table (account, blacklist)
+        VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_blacklist_follows_table_un
+        DO NOTHING;`,
+      [follower, following[i]]
+    )
+  }
 }
-const unfollowBlacklist = async (follow) => {
-  await pool.query(
-    `DELETE FROM hafsql.blacklist_follows_table
-      WHERE account=$1 AND blacklist=$2;`,
-    [follow.follower, follow.following]
-  )
+const unfollowBlacklist = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `DELETE FROM hafsql.blacklist_follows_table
+        WHERE account=$1 AND blacklist=$2;`,
+      [follower, following[i]]
+    )
+  }
 }
-const followMuted = async (follow) => {
-  await pool.query(
-    `INSERT INTO hafsql.mute_follows_table (account, mute_list)
-      VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_mute_follows_table_un
-      DO NOTHING;`,
-    [follow.follower, follow.following]
-  )
+const followMuted = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `INSERT INTO hafsql.mute_follows_table (account, mute_list)
+        VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_mute_follows_table_un
+        DO NOTHING;`,
+      [follower, following[i]]
+    )
+  }
 }
-const unfollowMuted = async (follow) => {
-  await pool.query(
-    `DELETE FROM hafsql.mute_follows_table
-      WHERE account=$1 AND mute_list=$2;`,
-    [follow.follower, follow.following]
-  )
+const unfollowMuted = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `DELETE FROM hafsql.mute_follows_table
+        WHERE account=$1 AND mute_list=$2;`,
+      [follower, following[i]]
+    )
+  }
 }
-const actualFollow = async (follow) => {
+const actualFollow = async () => {
+  let queryString = ''
+  let first = true
+  for (let i = 0; i < followersArray.length; i++) {
+    const temp = followersArray[i]
+    if (typeof temp === 'undefined') {
+      continue
+    }
+    if (!first) {
+      queryString += ','
+    }
+    queryString += `(${temp.follower}, ${temp.following})`
+    if (first) {
+      first = false
+    }
+  }
   await pool.query(
     `INSERT INTO hafsql.follows_table (follower, following)
-      VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_follows_table_un
-      DO NOTHING;`,
-    [follow.follower, follow.following]
+      VALUES ${queryString} ON CONFLICT ON CONSTRAINT hafsql_follows_table_un
+      DO NOTHING;`
   )
 }
-const mute = async (follow) => {
-  await pool.query(
-    `INSERT INTO hafsql.mutes_table (muter, muted)
-      VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_mutes_table_un
-      DO NOTHING;`,
-    [follow.follower, follow.following]
-  )
+const mute = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `INSERT INTO hafsql.mutes_table (muter, muted)
+        VALUES ($1, $2) ON CONFLICT ON CONSTRAINT hafsql_mutes_table_un
+        DO NOTHING;`,
+      [follower, following[i]]
+    )
+  }
 }
 // TODO: After Posts/Comments table - need to verify posts
 const insertReblog = async (follow) => {}
 
-const unfollowUnmute = async (follow) => {
-  await pool.query(
-    `DELETE FROM hafsql.follows_table
-      WHERE follower=$1 AND following=$2;`,
-    [follow.follower, follow.following]
-  )
-  await pool.query(
-    `DELETE FROM hafsql.mutes_table
-      WHERE muter=$1 AND muted=$2;`,
-    [follow.follower, follow.following]
-  )
+const unfollowUnmute = async (item) => {
+  const { follower, following } = item
+  for (let i = 0; i < following; i++) {
+    await pool.query(
+      `DELETE FROM hafsql.follows_table
+        WHERE follower=$1 AND following=$2;`,
+      [follower, following[i]]
+    )
+    await pool.query(
+      `DELETE FROM hafsql.mutes_table
+        WHERE muter=$1 AND muted=$2;`,
+      [follower, following[i]]
+    )
+  }
 }
 
 const resetBlacklist = async (follow) => {
