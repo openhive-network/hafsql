38100c58292b85098f3374bf337feb23ee792fa6
Mahdi Yari <m.yary15@gmail.com>
2023-05-20 20:59:10 +0330

    Update follows.js

--- helpers/syncs/follows.js
+++ helpers/syncs/follows.js
@@ -27,7 +27,7 @@ export const syncFollows = async () => {
 
 // full - single row inserts 228.3 minutes ~3.8 hours
 // full - multi row inserts TBD
-export const fillFollows = async (limit = 40000) => {
+export const fillFollows = async (limit = 50000) => {
   let start = await pool.query(
     'SELECT last_op_id FROM hafsql.sync_data WHERE table_name=$1;',
     ['follows']
