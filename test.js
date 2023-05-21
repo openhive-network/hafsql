a12b6ffc3e4413fc805dcb40dd9e06c76b937419
Mahdi Yari <m.yary15@gmail.com>
2023-05-21 21:29:00 +0330

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { pool } from './helpers/database.js'
 
 const test = async () => {
   let size = 0
-  const res = await pool.query('SELECT length(body) as test1 FROM hafsql."TxComment" x order by op_id DESC LIMIT 100000')
+  const res = await pool.query('SELECT length(body) as test1 FROM hafsql."TxComment" x order by op_id asc LIMIT 100000')
   for (let i = 0; i < res.rowCount; i++) {
     size += res.rows[i].test1
   }
