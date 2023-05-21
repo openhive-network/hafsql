bc6236869ab49078e9fbad13e6b88a810fc53969
Mahdi Yari <m.yary15@gmail.com>
2023-05-21 21:24:02 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { pool } from './helpers/database.js'
 
 const test = async () => {
-  const res = await pool.query('SELECT op_id, voter, proposal_ids, approve FROM hafsql."TxUpdateProposalVotes" LIMIT 10')
+  const res = await pool.query('SELECT length(body) FROM hafsql."TxComment" x order by op_id DESC LIMIT 100000')
   console.log(res.rows[0])
 }
 test()
