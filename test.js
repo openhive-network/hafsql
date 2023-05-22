92652a5011dcda3bb2991e84080dfab9abfe9686
Mahdi Yari <m.yary15@gmail.com>
2023-05-22 19:06:52 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 import { pool } from './helpers/database.js'
 import { applyPatches, parsePatch } from '@sanity/diff-match-patch'
 
+const res = await pool.query(
+  'SELECT x.body FROM hafsql."TxComment" x WHERE author =$1 and permlink=$2 ORDER BY op_id ASC',
+  ['mahdiyari', 'hive-twitter-community']
+)
+
 const test = async () => {
-  const res = await pool.query(
-    'SELECT x.body FROM hafsql."TxComment" x WHERE author =$1 and permlink=$2 ORDER BY op_id ASC',
-    ['mahdiyari', 'hive-twitter-community']
-  )
   let original = res.rows[0].body
   for (let i = 1; i < res.rowCount; i++) {
     const [temp] = applyPatches(parsePatch(res.rows[i].body), original)
@@ -13,4 +14,9 @@ const test = async () => {
   }
   console.log(original)
 }
-test()
+
+const nowTime = Date.now()
+for (let i = 0; i < 1000000; i++) {
+  test()
+}
+console.log((Date.now() - nowTime))
