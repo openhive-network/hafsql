8fbb508735e8a117511b4f48db5d4c72b29e7a40
Mahdi Yari <m.yary15@gmail.com>
2023-05-22 23:12:01 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 import { pool } from './helpers/database.js'
-import DiffMatchPatch from 'diff-match-patch'
+// import DiffMatchPatch from 'diff-match-patch'
+import { diff_match_patch as Dmp } from './helpers/dmp.js'
 
 const res = await pool.query(
   'SELECT x.body FROM hafsql."TxComment" x WHERE author =$1 and permlink=$2 ORDER BY op_id ASC',
   ['mahdiyari', 'hive-twitter-community']
 )
 
+// 20s
 // const test = async () => {
 //   let original = res.rows[0].body
 //   for (let i = 1; i < res.rowCount; i++) {
@@ -14,8 +16,39 @@ const res = await pool.query(
 //   }
 //   // console.log(original)
 // }
+
+// 11s
+// const test = async () => {
+//   const dmp = new DiffMatchPatch()
+//   let original = res.rows[0].body
+//   for (let i = 1; i < res.rowCount; i++) {
+//     const patch = dmp.patch_fromText(res.rows[i].body)
+//     const [temp] = dmp.patch_apply(patch, original)
+//     original = temp
+//   }
+//   // console.log(original)
+// }
+
+// const original = `<center>![hivetwitter3.jpg](https://images.hive.blog/DQmUim2ktqCow2F2wtNKT6L2VUFARvH5B5JQs5dVGCrgsvB/hivetwitter3.jpg)</center>
+// Short tweets.
+// Max 280 characters.
+// For longer tweets and threads use comments under your tweet post.
+// (everything else just like twitter.com)
+
+// Let's make it fun. Please follow the rules.
+
+// Also looking for MODs to remove long blogs submitted to the community.`
+
+// const p1 = `@@ -377,8 +377,47 @@
+//  mmunity.
+// +%0A%0Ahttps://hive.blog/created/hive-113882
+// `
+
 const test = async () => {
-  const dmp = new DiffMatchPatch()
+  const dmp = new Dmp()
+  // const patch = dmp.patch_fromText(p1)
+  // const [temp] = dmp.patch_apply(patch, original)
+  // console.log(temp)
   let original = res.rows[0].body
   for (let i = 1; i < res.rowCount; i++) {
     const patch = dmp.patch_fromText(res.rows[i].body)
@@ -26,7 +59,7 @@ const test = async () => {
 }
 
 const nowTime = Date.now()
-for (let i = 0; i < 20000; i++) {
+for (let i = 0; i < 1000000; i++) {
   test()
 }
 console.log((Date.now() - nowTime))
