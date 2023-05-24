22d966ef521de0c8607b803e4882fa65de095298
Mahdi Yari <m.yary15@gmail.com>
2023-05-24 23:59:04 +0330

    Update comments.js

--- helpers/syncs/comments.js
+++ helpers/syncs/comments.js
@@ -53,8 +53,8 @@ const insertComments = async (items) => {
     if (!first) {
       queryString += ','
     }
-    const body = comment.bodyEdited ? comment.body : "''"
-    queryString += `(${comment.author},${comment.permlink},${comment.op_id},${comment.bodyEdited},${body},'${JSON.stringify(comment.tags)}'::jsonb)`
+    const body = comment.bodyEdited ? comment.body : ''
+    queryString += `('${comment.author}','${comment.permlink}',${comment.op_id},${comment.bodyEdited},'${body}','${JSON.stringify(comment.tags)}'::jsonb)`
     if (first) {
       first = false
     }
