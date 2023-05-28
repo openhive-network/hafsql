11e0f45775469a91208f43e663af44e024cfbc13
Mahdi Yari <m.yary15@gmail.com>
2023-05-28 03:52:46 +0330

    Update comments.js

--- helpers/syncs/comments.js
+++ helpers/syncs/comments.js
@@ -68,6 +68,7 @@ const insertComments = async (items) => {
     }
   }
   if (params.length > 0) {
+    console.log(params[17455])
     await pool.query(`INSERT INTO hafsql.comments_table (author, permlink, last_op_id, body_edited, body, tags, created) VALUES ${queryString};`, params)
   }
 }
