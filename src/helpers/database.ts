d0066aefc593b986d0d1fdb3b2414faf38cdc508
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-07-25 11:36:31 +0330

    Create database.ts

---
+++ src/helpers/database.ts
@@ -0,0 +1,17 @@
+import { Pool } from '../deps.ts'
+
+// Per worker
+// We have 8 workers = 2 * 8 = 16 connections
+// Recommended 2
+const POOL_SIZE = 2
+
+export const pool = new Pool(
+	{
+		hostname: '172.17.0.2',
+		port: 5432,
+		database: 'haf_block_log',
+		user: 'haf_admin',
+	},
+	POOL_SIZE,
+	true,
+)
