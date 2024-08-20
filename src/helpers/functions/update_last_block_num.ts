c4b347236f3d18b3a5891997556e2dbca489f660
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-08-20 19:23:34 +0330

    Create update_last_block_num.ts

---
+++ src/helpers/functions/update_last_block_num.ts
@@ -0,0 +1,13 @@
+import { PoolClient } from '../../deps.ts'
+import { Transaction } from '../../deps.ts'
+
+export const updateLastBlockNum = async (
+  table: string,
+  blockNum: number,
+  trx: Transaction | PoolClient,
+) => {
+  await trx.queryObject(
+    'UPDATE hafsql.sync_data SET last_block_num=$1 WHERE table_name=$2;',
+    [blockNum, table],
+  )
+}
