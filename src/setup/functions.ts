d0b5d55616b8a574527c33a52046f9e65432a0a8
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-08-20 19:24:41 +0330

    Update functions.ts

--- src/setup/functions.ts
+++ src/setup/functions.ts
@@ -4,28 +4,29 @@ export const setupFunctions = async () => {
   // `using` so don't need to release the client
   using client = await pool.connect()
 
+  // hafsql.asset_amount(text)
   // Get the asset amount from the asset object
-  const assetAmount = `CREATE OR REPLACE FUNCTION hafsql.asset_amount(text)
-    RETURNS float
+  await client.queryObject(`CREATE OR REPLACE FUNCTION hafsql.asset_amount(text)
+    RETURNS numeric
     AS $$
     DECLARE
-      amount float;
+      amount numeric;
     BEGIN
-      SELECT ($1::jsonb->>'amount')::int8 / power(10, ($1::jsonb->>'precision')::int8) INTO STRICT amount;
-      RETURN amount;
+      SELECT ($1::jsonb->>'amount')::numeric / power(10, ($1::jsonb->>'precision')::numeric) INTO STRICT amount;
+      RETURN amount::numeric;
     EXCEPTION WHEN OTHERS THEN
       RETURN 0;
     END
     $$
     LANGUAGE plpgsql
     IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(assetAmount)
+    RETURNS NULL ON NULL INPUT;`)
   await client.queryObject(`COMMENT ON FUNCTION hafsql.asset_amount (text) IS
     'Return the calculated amount(float) of an asset object like {"nai": "@@000000021", "amount": "2123", "precision": 3}';`)
 
+  // hafsql.asset_symbol(text)
   // Get the asset symbol from the asset object
-  const assetSymbol = `CREATE OR REPLACE FUNCTION hafsql.asset_symbol(text)
+  await client.queryObject(`CREATE OR REPLACE FUNCTION hafsql.asset_symbol(text)
     RETURNS text
     AS $$
     DECLARE
@@ -42,13 +43,13 @@ export const setupFunctions = async () => {
     $$
     LANGUAGE plpgsql
     IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(assetSymbol)
+    RETURNS NULL ON NULL INPUT;`)
   await client.queryObject(`COMMENT ON FUNCTION hafsql.asset_symbol (text) IS
     'Return the symbol(text) of an asset object like {"nai": "@@000000021", "amount": "2123", "precision": 3}';`)
 
+  // hafsql.is_json(text)
   // Check if the string is valid json
-  const isJson = `CREATE OR REPLACE FUNCTION hafsql.is_json(text)
+  await client.queryObject(`CREATE OR REPLACE FUNCTION hafsql.is_json(text)
     RETURNS bool
     AS $$
     BEGIN
@@ -59,104 +60,155 @@ export const setupFunctions = async () => {
     $$
     LANGUAGE plpgsql
     IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(isJson)
+    RETURNS NULL ON NULL INPUT;`)
   await client.queryObject(`COMMENT ON FUNCTION hafsql.is_json (text) IS
     'Test if input text is valid JSON. Returns true, false, or NULL on NULL input.';`)
 
+  // hafsql.vests_to_hive(numeric, int4 default NULL)
   // VESTS to HIVE equivalent
-  const vestsToHive = `CREATE OR REPLACE FUNCTION hafsql.vests_to_hive(text)
-    RETURNS numeric(12, 3)
-    AS $$
-    DECLARE
-      hive_amount numeric;
-    BEGIN
-      SELECT $1::numeric / power(10, 6) / vests_per_hive FROM hafsql.dynamic_global_properties ORDER BY block_num DESC LIMIT 1
-      INTO STRICT hive_amount;
-      RETURN hive_amount::numeric(12, 3);
-    EXCEPTION WHEN OTHERS THEN
-      RETURN 0;
-    END
-    $$
-    LANGUAGE plpgsql
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(vestsToHive)
-  await client.queryObject(`COMMENT ON FUNCTION hafsql.vests_to_hive (text) IS
-    'Return HIVE equivalent of the VESTS or RC';`)
-
-  // VESTS to HIVE at certain block
-  const vestsToHiveAtBlock =
-    `CREATE OR REPLACE FUNCTION hafsql.vests_to_hive(text, int4)
-    RETURNS numeric(12, 3)
-    AS $$
-    DECLARE
-      hive_amount numeric;
-    BEGIN
-      SELECT $1::numeric / power(10, 6) / vests_per_hive FROM hafsql.dynamic_global_properties WHERE block_num = $2 LIMIT 1
-      INTO STRICT hive_amount;
-      RETURN hive_amount::numeric(12, 3);
-    EXCEPTION WHEN OTHERS THEN
-      RETURN 0;
-    END
-    $$
-    LANGUAGE plpgsql
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(vestsToHiveAtBlock)
   await client.queryObject(
-    `COMMENT ON FUNCTION hafsql.vests_to_hive (text, int4) IS
-    'Return HIVE equivalent of the VESTS or RC at certain block_num';`,
-  )
-
-  // Duplicate functions of vests_to_hive with the type numeric
-  // VESTS to HIVE equivalent - numeric
-  const vestsToHiveNumeric =
-    `CREATE OR REPLACE FUNCTION hafsql.vests_to_hive(numeric)
-    RETURNS numeric(12, 3)
+    `CREATE OR REPLACE FUNCTION hafsql.vests_to_hive(numeric, int4 default NULL)
+    RETURNS numeric(15, 3)
     AS $$
     DECLARE
-      hive_amount numeric;
+      hive_amount numeric(15, 3);
     BEGIN
-      SELECT $1::numeric / power(10, 6) / vests_per_hive FROM hafsql.dynamic_global_properties ORDER BY block_num DESC LIMIT 1
+      SELECT CASE WHEN $2 is NULL THEN
+        (SELECT $1 / vests_per_hive FROM hafsql.dynamic_global_properties ORDER BY block_num DESC LIMIT 1)
+      ELSE
+        (SELECT $1 / vests_per_hive FROM hafsql.dynamic_global_properties WHERE block_num = $2 LIMIT 1)
+      END
       INTO STRICT hive_amount;
-      RETURN hive_amount::numeric(12, 3);
+      RETURN hive_amount::numeric(15, 3);
     EXCEPTION WHEN OTHERS THEN
       RETURN 0;
     END
     $$
     LANGUAGE plpgsql
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(vestsToHiveNumeric)
-  await client.queryObject(
-    `COMMENT ON FUNCTION hafsql.vests_to_hive (numeric) IS
-    'Return HIVE equivalent of the VESTS or RC';`,
+    IMMUTABLE;`,
   )
-
-  // VESTS to HIVE at certain block - numeric
-  const vestsToHiveAtBlockNumeric =
-    `CREATE OR REPLACE FUNCTION hafsql.vests_to_hive(numeric, int4)
-    RETURNS numeric(12, 3)
-    AS $$
-    DECLARE
-      hive_amount numeric;
-    BEGIN
-      SELECT $1::numeric / power(10, 6) / vests_per_hive FROM hafsql.dynamic_global_properties WHERE block_num = $2 LIMIT 1
-      INTO STRICT hive_amount;
-      RETURN hive_amount::numeric(12, 3);
-    EXCEPTION WHEN OTHERS THEN
-      RETURN 0;
-    END
-    $$
-    LANGUAGE plpgsql
-    IMMUTABLE
-    RETURNS NULL ON NULL INPUT;`
-  await client.queryObject(vestsToHiveAtBlockNumeric)
   await client.queryObject(
     `COMMENT ON FUNCTION hafsql.vests_to_hive (numeric, int4) IS
-    'Return HIVE equivalent of the VESTS or RC at certain block_num';`,
+    'Return HIVE equivalent of the VESTS optionally at certain block_num';`,
   )
 
-  // TODO: fix this mess - get rid of all nai assets
+  // hafsql.rc_to_hive(numeric, int4 default NULL)
+  // RC to HIVE equivalent
+  await client.queryObject(
+    `CREATE OR REPLACE FUNCTION hafsql.rc_to_hive(numeric, int4 default NULL)
+    RETURNS numeric(15, 3)
+    AS $$
+    BEGIN
+      RETURN hafsql.vests_to_hive(($1 / power(10, 6))::numeric, $2);
+    END
+    $$
+    LANGUAGE plpgsql
+    IMMUTABLE;`,
+  )
+  await client.queryObject(
+    `COMMENT ON FUNCTION hafsql.rc_to_hive (numeric, int4) IS
+    'Return HIVE equivalent of the RC optionally at certain block_num';`,
+  )
+
+  // hafsql.get_trx_id(int8)
+  // Get trx_id from op_id
+  await client.queryObject(`CREATE OR REPLACE FUNCTION hafsql.get_trx_id(int8)
+    RETURNS text
+    AS $$
+    DECLARE
+      trxid text;
+    BEGIN
+      SELECT t.trx_id FROM hafsql.operations o
+        JOIN hafsql.transactions t
+          ON t.block_num = o.block_num
+          AND t.trx_in_block = o.trx_in_block
+        WHERE o.id = $1
+        INTO trxid;
+      RETURN trxid;
+    EXCEPTION WHEN OTHERS THEN
+      RETURN NULL;
+    END
+    $$
+    LANGUAGE plpgsql
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`)
+  await client.queryObject(`COMMENT ON FUNCTION hafsql.get_trx_id (int8) IS
+    'Return trx_id from op_id';`)
+
+  // hafsql.to_json(text)
+  // Cast json string into JSONB - return null on invalid json
+  await client.queryObject(`CREATE OR REPLACE FUNCTION hafsql.to_json(text)
+    RETURNS jsonb
+    AS $$
+    DECLARE
+      jsonified jsonb;
+    BEGIN
+      SELECT CASE WHEN hafsql.is_json($1) THEN $1::jsonb ELSE NULL END
+        INTO jsonified;
+      RETURN jsonified;
+    EXCEPTION WHEN OTHERS THEN
+      RETURN NULL;
+    END
+    $$
+    LANGUAGE plpgsql
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`)
+  await client.queryObject(
+    `COMMENT ON FUNCTION hafsql.to_json (text) IS
+    'Cast valid json TEXT into JSONB and return NULL on invalid json string';`,
+  )
+
+  // hafsql.last_op_id_from_block_num(int4)
+  // Get highest op_id inside a block
+  await client.queryObject(
+    `CREATE OR REPLACE FUNCTION hafsql.last_op_id_from_block_num(int4)
+    RETURNS int8
+    AS $$
+    BEGIN
+      RETURN (SELECT id FROM hive.operations
+        WHERE hive.operation_id_to_block_num(id) = $1
+        ORDER BY id DESC limit 1);
+    EXCEPTION WHEN OTHERS THEN
+      RETURN NULL;
+    END
+    $$
+    LANGUAGE plpgsql
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`,
+  )
+  await client.queryObject(
+    `COMMENT ON FUNCTION hafsql.last_op_id_from_block_num (int4) IS
+    'Get highest op_id inside a block';`,
+  )
+
+  // hafsql.get_next_block_range(text)
+  // Return a range of 49999 block numbers e.g. [1, 50000]
+  // Probably can increase - have to test
+  await client.queryObject(
+    `CREATE OR REPLACE FUNCTION hafsql.get_next_block_range(text)
+    RETURNS int8
+    AS $$
+    DECLARE
+      last_num int4;
+      head_num int4;
+    BEGIN
+      SELECT num INTO STRICT head_num FROM hive.blocks ORDER BY num DESC LIMIT 1;
+      SELECT last_block_num INTO STRICT last_num FROM hafsql.sync_data WHERE name = $1;
+      RETURN
+        CASE WHEN head_num > last_num THEN
+          CASE WHEN head_num >= last_num + 50000
+            THEN ARRAY[last_num + 1, last_num + 50000]
+            ELSE ARRAY[last_num + 1, head_num]
+          END
+        ELSE
+          NULL
+        END;
+    EXCEPTION WHEN OTHERS THEN
+      RETURN NULL;
+    END
+    $$
+    LANGUAGE plpgsql
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;`,
+  )
 }
