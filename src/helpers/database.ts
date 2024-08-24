a9a21a2ac9919b426accde36808a66cc51e89a3f
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-08-24 05:06:02 +0330

    Update database.ts

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -3,7 +3,7 @@ import { Pool } from '../deps.ts'
 // Per worker
 // We have 8 workers = 2 * 8 = 16 connections
 // Recommended 2
-const POOL_SIZE = 2
+const POOL_SIZE = Number(Deno.env.get('HAFSQL_PGPOOLSIZE')) || 3
 
 export const pool = new Pool(
 	{
