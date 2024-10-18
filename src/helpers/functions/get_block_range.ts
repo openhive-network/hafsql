d7c7c4b7decb56d5f805b3ed81b969d6d412cfc8
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-10-18 12:56:42 +0330

    Update get_block_range.ts

--- src/helpers/functions/get_block_range.ts
+++ src/helpers/functions/get_block_range.ts
@@ -5,12 +5,13 @@ import { BlockRange } from '../types.ts'
 export const getBlockRange = async (
   table: string,
   _client?: PoolClient | Transaction,
+  range = 50000,
 ) => {
   if (!_client) {
     using client = await pool.connect()
     const blockRangeQ = await client.queryObject<BlockRange>(
-      'SELECT hafsql.get_next_block_range($1) as block_range;',
-      [table],
+      'SELECT hafsql.get_next_block_range($1, $2) as block_range;',
+      [table, range],
     )
     if (blockRangeQ.rows.length < 1) {
       return null
@@ -18,8 +19,8 @@ export const getBlockRange = async (
     return blockRangeQ.rows[0].block_range
   } else {
     const blockRangeQ = await _client.queryObject<BlockRange>(
-      'SELECT hafsql.get_next_block_range($1) as block_range;',
-      [table],
+      'SELECT hafsql.get_next_block_range($1, $2) as block_range;',
+      [table, range],
     )
     if (blockRangeQ.rows.length < 1) {
       return null
