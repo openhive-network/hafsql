254b9b405f971442d3052983bb90bda9fd2bd1a8
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-10-18 12:57:53 +0330

    Update setup.ts

--- src/setup/setup.ts
+++ src/setup/setup.ts
@@ -1,14 +1,18 @@
 import { sleep } from '../helpers/functions/sleep.ts'
 import { removeExtraViews, setupExtraViews } from './extra_views.ts'
 import { setupFunctions } from './functions.ts'
-import { removeOperationViews, setupOperationViews } from './operations.ts'
-import { setupPublicUser } from './public_user.ts'
-import { setupSchema } from './schema.ts'
-import { setupTables } from './tables.ts'
+import { setupOperationTables } from './setup_operation_tables.ts'
+import {
+	removeOperationViews,
+	setupOperationViews,
+} from './setup_operations_views.ts'
+import { setupPublicUser } from './setup_public_user.ts'
+import { setupSchema } from './setup_schema.ts'
+import { setupTables } from './setup_tables.ts'
 import {
 	removeVirtualOperationViews,
 	setupVirtualOperationViews,
-} from './virtual_operations.ts'
+} from './setup_virtual_operations_views.ts'
 
 export const setup = async (): Promise<void> => {
 	try {
@@ -17,6 +21,8 @@ export const setup = async (): Promise<void> => {
 		await setupFunctions()
 
 		await setupTables()
+
+		await setupOperationTables()
 
 		// Remove the views to recreate them in case they have changed
 		await removeExtraViews()
