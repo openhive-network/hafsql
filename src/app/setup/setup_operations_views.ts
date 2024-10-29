93b02c66660bf77450c09d83225171db5cafcd4c
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-10-29 23:12:47 +0330

    Create setup_operations_views.ts

---
+++ src/app/setup/setup_operations_views.ts
@@ -0,0 +1,102 @@
+import { pool } from '../helpers/database.ts'
+
+const param = (param: string, jsonb = false) => {
+  if (jsonb) {
+    return `(o.body_binary::jsonb->'value'->'${param}')`
+  }
+  return `(o.body_binary::jsonb->'value'->>'${param}')`
+}
+const amount = (param: string) => {
+  return `hafsql.asset_amount(${param})`
+}
+const symbol = (param: string) => {
+  return `hafsql.asset_symbol(${param})`
+}
+const block = (id: string) => {
+  return `hive.operation_id_to_block_num(${id})`
+}
+
+const OPs = 49
+
+// Create views for the operations that don't have tables
+export const setupOperationViews = async () => {
+  using client = await pool.connect()
+  // No dedicated table
+  const OpVote = `CREATE OR REPLACE VIEW hafsql.operation_vote_view
+    AS SELECT o.id,
+      hb.created_at AS "timestamp",
+      ${param('voter')} AS "voter",
+      ${param('author')} AS "author",
+      ${param('weight')}::int4 AS "weight",
+      ${param('permlink')} AS "permlink",
+      ${block('o.id')} AS "block_num",
+      hafsql.get_trx_id(o.id) AS "trx_id"
+    FROM hive.operations o
+    JOIN hive.blocks hb ON hb.num = ${block('o.id')}
+    WHERE hive.operation_id_to_type_id(o.id) = 0;`
+  await client.queryObject(OpVote)
+
+  // No dedicated table
+  const OpComment = `CREATE OR REPLACE VIEW hafsql.operation_comment_view
+  AS SELECT o.id,
+    hb.created_at AS "timestamp",
+    ${param('author')} AS "author",
+    ${param('permlink')} AS "permlink",
+    ${param('parent_author')}::text AS "parent_author",
+    ${param('parent_permlink')} AS "parent_permlink",
+    ${param('title')} AS "title",
+    ${param('body')} AS "body",
+    hafsql.to_json(${param('json_metadata')}) AS "json_metadata",
+    ${block('o.id')} AS "block_num",
+    hafsql.get_trx_id(o.id) AS "trx_id"
+    FROM hive.operations o
+    JOIN hive.blocks hb ON hb.num = ${block('o.id')}
+    WHERE hive.operation_id_to_type_id(o.id) = 1;`
+  await client.queryObject(OpComment)
+
+  // No dedicated table
+  const OpCustomJson = `CREATE OR REPLACE VIEW hafsql.operation_custom_json_view
+  AS SELECT o.id,
+    hb.created_at AS "timestamp",
+    ${param('required_auths', true)} AS "required_auths",
+    ${param('required_posting_auths', true)} AS "required_posting_auths",
+    ${param('id')} AS "custom_id",
+    ${param('json')} AS "json",
+    ${block('o.id')} AS "block_num",
+    hafsql.get_trx_id(o.id) AS "trx_id"
+    FROM hive.operations o
+    JOIN hive.blocks hb ON hb.num = ${block('o.id')}
+    WHERE hive.operation_id_to_type_id(o.id) = 18;`
+  await client.queryObject(OpCustomJson)
+
+  // +23
+  // No dedicated table
+  const VOEffectiveCommentVote =
+    `CREATE OR REPLACE VIEW hafsql.operation_effective_comment_vote_view
+    AS SELECT o.id,
+      hb.created_at AS "timestamp",
+      ${param('voter')} AS voter,
+      ${param('author')} AS author,
+      ${param('permlink')} AS permlink,
+      ${param('weight')} AS weight,
+      ${param('rshares')}::numeric AS rshares,
+      ${param('total_vote_weight')} AS total_vote_weight,
+      ${amount(param('pending_payout'))} AS pending_payout,
+      ${symbol(param('pending_payout'))} AS pending_payout_symbol,
+      ${block('o.id')} as block_num
+    FROM hive.operations o
+    JOIN hive.blocks hb ON hb.num = ${block('o.id')}
+    WHERE hive.operation_id_to_type_id(o.id) = ${OPs} + 23;`
+  await client.queryObject(VOEffectiveCommentVote)
+}
+
+export const removeOperationViews = async () => {
+  using client = await pool.connect()
+  const dropViews = `DROP VIEW IF EXISTS
+    hafsql.operation_vote_view,
+    hafsql.operation_comment_view,
+    hafsql.operation_custom_json_view,
+    hafsql.operation_effective_comment_vote_view
+    CASCADE;`
+  await client.queryObject(dropViews)
+}
