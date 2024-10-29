f9f754f9567f2450fde8b025f33f48acee418f1b
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-10-29 23:12:15 +0330

    Create get_user_id.ts

---
+++ src/app/helpers/functions/get_user_id.ts
@@ -0,0 +1,39 @@
+import { PoolClient, Transaction } from '../../../deps.ts'
+import { pool } from '../database.ts'
+import { clearUsername } from './validate_username.ts'
+
+let accountCache: Record<string, number> = {}
+
+const clearCache = () => {
+  accountCache = {}
+}
+// every 10min clear cache
+setInterval(() => clearCache(), 600000)
+
+// Caching ids for duration of the sync
+export const getUserId = async (username: string, trx?: Transaction) => {
+  username = clearUsername(username)
+  if (Object.hasOwn(accountCache, username)) {
+    return accountCache[username]
+  } else {
+    let client
+    if (trx) {
+      client = trx
+    } else {
+      client = await pool.connect()
+    }
+    const getId = await client.queryObject<{ id: number }>(
+      'SELECT a.id FROM hive.accounts a WHERE a.name=$1',
+      [username],
+    )
+    if (!trx && client instanceof PoolClient) {
+      client.release()
+    }
+    if (getId.rows.length < 1) {
+      return null
+    }
+    const id = getId.rows[0].id
+    accountCache[username] = id
+    return id
+  }
+}
