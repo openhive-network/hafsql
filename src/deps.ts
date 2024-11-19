001bfe72f3ccfacbb6104619179dc1597053c054
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-11-19 10:11:52 +0330

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,25 +1,31 @@
+// All external dependencies go here for easier maintenance
+
 export {
   Pool,
   PoolClient,
   Transaction,
 } from 'https://deno.land/x/postgres@v0.19.3/mod.ts'
-export { load as loadDotEnv } from 'https://deno.land/std@0.224.0/dotenv/mod.ts'
+export { load as loadDotEnv } from '@std/dotenv'
 export { BigDenary } from 'https://deno.land/x/bigdenary@1.0.0/mod.ts'
 export {
   Application,
+  Context,
   isHttpError,
   Router,
   Status,
   STATUS_TEXT,
-} from 'jsr:@oak/oak@^17'
-// export { Controller, DanetApplication, Get } from 'jsr:@danet/core@^2'
-export { default as swaggerJsDoc } from 'npm:swagger-jsdoc@6.2.8'
-export { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts'
+} from '@oak/oak'
+export { oakCors } from '@tajpouria/cors'
+import openapi, { ParserOptions } from 'oa-parser'
+// import openapi, { ParserOptions } from 'openapi-comment-parser'
 
 // @deno-types="npm:@types/diff-match-patch@1.0.36"
-import diff_match_patch from 'npm:diff-match-patch@1.0.5'
+import diff_match_patch from 'diff-match-patch'
 // @deno-types="npm:@types/json-bigint@1.0.4"
-import JSONbig from 'npm:json-bigint@1.0.0'
+import JSONbig from 'json-bigint'
 
 export const DiffMatchPatch = diff_match_patch
-export const JSONBigInt = JSONbig
+export const BigJSONparser = JSONbig({ storeAsString: true }).parse
+export const BigJSONstringifier = JSONbig({ storeAsString: true }).stringify
+export const openApi = openapi
+export type ApiParserOptions = ParserOptions
