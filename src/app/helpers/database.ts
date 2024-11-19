20eaef7496dc1471f4a70dd3c4693c0ed7a455a2
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-11-19 10:10:29 +0330

    Update database.ts

--- src/app/helpers/database.ts
+++ src/app/helpers/database.ts
@@ -1,16 +1,18 @@
 import { Pool } from '../../deps.ts'
 
-// Per worker
-// We have 8 workers = 2 * 8 = 16 connections
-// Recommended 2
-const POOL_SIZE = Number(Deno.env.get('HAFSQL_PGPOOLSIZE')) || 3
+// Lazy loaded per worker
+const POOL_SIZE = Number(Deno.env.get('HAFSQL_PGPOOLSIZE')) || 5
+const PG_HOST = Deno.env.get('HAFSQL_PGHOST') || '172.17.0.2'
+const PG_PORT = Number(Deno.env.get('HAFSQL_PGPORT')) || 5432
+const PG_DATABASE = Deno.env.get('HAFSQL_PGDATABASE') || 'haf_block_log'
+const PG_USER = Deno.env.get('HAFSQL_PGUSER') || 'haf_admin'
 
 export const pool = new Pool(
 	{
-		hostname: '172.17.0.2',
-		port: 5432,
-		database: 'haf_block_log',
-		user: 'haf_admin',
+		hostname: PG_HOST,
+		port: PG_PORT,
+		database: PG_DATABASE,
+		user: PG_USER,
 		applicationName: 'hafsql',
 	},
 	POOL_SIZE,
