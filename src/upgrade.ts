b2c89f3c183f0d99b2d6f9e548aaa2eaea485047
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:55:08 +0330

    Update upgrade.ts

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -1,18 +1,17 @@
 import deno from '../deno.json' with { type: 'json' }
-import { pool } from './app/helpers/database.ts'
+import { query } from './app/helpers/database.ts'
 
 /**
  * Make upgrade decisions based on the version
  */
 export const handleUpgrade = async () => {
-	const client = await pool.connect()
-	const getVersion = await client.queryObject<{ version: string }>(
+	const getVersion = await query<{ version: string }>(
 		'SELECT version FROM hafsql.version WHERE name=$1',
 		['hafsql'],
 	)
 	if (getVersion.rows.length === 0) {
 		// not an upgrade
-		await client.queryObject(
+		await query(
 			'INSERT INTO hafsql.version (name, version) VALUES ($1, $2)',
 			['hafsql', deno.version],
 		)
@@ -26,10 +25,9 @@ export const handleUpgrade = async () => {
 	// do something
 	// }
 	if (oldVersion !== newVersion) {
-		await client.queryObject(
+		await query(
 			'UPDATE hafsql.version SET version=$1 WHERE name=$2',
 			[newVersion, 'hafsql'],
 		)
 	}
-	client.release()
 }
