c64729006f0b1b9eccbe6108e45522f3f25ce4e9
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:54:06 +0330

    Update get_post_id.ts

--- src/app/helpers/utils/get_post_id.ts
+++ src/app/helpers/utils/get_post_id.ts
@@ -1,28 +1,27 @@
-import { pool } from '../database.ts'
+import { query } from '../database.ts'
 
 let postCache: Record<string, number> = {}
 
 const clearCache = () => {
-  postCache = {}
+	postCache = {}
 }
 // every 10min clear cache
 setInterval(() => clearCache(), 600000)
 
 export const getPostId = async (author: string, permlink: string) => {
-  const postString = author + ';' + permlink
-  if (Object.hasOwn(postCache, postString)) {
-    return postCache[postString]
-  } else {
-    using client = await pool.connect()
-    const idQ = await client.queryObject<{ id: number }>(
-      'SELECT id FROM hafsql.comments_table WHERE author=$1 AND permlink=$2',
-      [author, permlink],
-    )
-    if (idQ.rows.length < 1) {
-      return null
-    }
-    const id = idQ.rows[0].id
-    postCache[postString] = id
-    return id
-  }
+	const postString = author + ';' + permlink
+	if (Object.hasOwn(postCache, postString)) {
+		return postCache[postString]
+	} else {
+		const idQ = await query<{ id: number }>(
+			'SELECT id FROM hafsql.comments_table WHERE author=$1 AND permlink=$2',
+			[author, permlink],
+		)
+		if (idQ.rows.length < 1) {
+			return null
+		}
+		const id = idQ.rows[0].id
+		postCache[postString] = id
+		return id
+	}
 }
