6999f93368c66259cbf2000a77247a2a44bfca7b
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:55:05 +0330

    Create purge.ts

---
+++ src/purge.ts
@@ -0,0 +1,18 @@
+import { query } from './app/helpers/database.ts'
+import { print } from './app/helpers/utils/print.ts'
+import { hiveIndexes } from './app/indexes/hive.ts'
+
+/**
+ * Remove hafsql schema and everything related
+ */
+export const purgeHafSQL = async () => {
+	print('Removing hafsql schema...')
+	await query('DROP SCHEMA IF EXISTS hafsql CASCADE;')
+	print('Removing indexes...')
+	for (let i = 0; i < hiveIndexes.length; i++) {
+		await query(
+			`DROP INDEX CONCURRENTLY IF EXISTS ${hiveIndexes[i].name}`,
+		)
+	}
+	print('Purge done.')
+}
