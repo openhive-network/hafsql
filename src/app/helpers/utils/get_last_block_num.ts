7eea74c1520836543f6e9bd2ba502686437e1437
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:54:05 +0330

    Update get_last_block_num.ts

--- src/app/helpers/utils/get_last_block_num.ts
+++ src/app/helpers/utils/get_last_block_num.ts
@@ -1,22 +1,12 @@
-import { PoolClient, Transaction } from '../../../deps.ts'
-import { pool } from '../database.ts'
+import { customClient, query } from '../database.ts'
 
 export const getLastBlockNum = async (
-  table: string,
-  _client?: Transaction | PoolClient,
+	table: string,
+	client: customClient = { query },
 ) => {
-  if (!_client) {
-    using client = await pool.connect()
-    const result = await client.queryObject<{ last_block_num: number }>(
-      'SELECT last_block_num FROM hafsql.sync_data WHERE table_name=$1;',
-      [table],
-    )
-    return result.rows[0].last_block_num
-  } else {
-    const result = await _client.queryObject<{ last_block_num: number }>(
-      'SELECT last_block_num FROM hafsql.sync_data WHERE table_name=$1;',
-      [table],
-    )
-    return result.rows[0].last_block_num
-  }
+	const result = await client.query<{ last_block_num: number }>(
+		'SELECT last_block_num FROM hafsql.sync_data WHERE table_name=$1;',
+		[table],
+	)
+	return result.rows[0].last_block_num
 }
