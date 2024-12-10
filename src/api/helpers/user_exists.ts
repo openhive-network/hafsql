f0507b1506489f62286eec50c313aef7a18ff472
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:52:39 +0330

    Update user_exists.ts

--- src/api/helpers/user_exists.ts
+++ src/api/helpers/user_exists.ts
@@ -1,10 +1,9 @@
-import { pool } from '../../app/helpers/database.ts'
+import { query } from '../../app/helpers/database.ts'
 
 export const userExists = async (username: string) => {
-  using client = await pool.connect()
-  const exists = await client.queryObject<{ exists: boolean }>(
-    'SELECT EXISTS (SELECT 1 FROM hafsql.accounts WHERE name=$1) AS exists',
-    [username],
-  )
-  return exists.rows[0].exists
+	const exists = await query<{ exists: boolean }>(
+		'SELECT EXISTS (SELECT 1 FROM hafsql.accounts WHERE name=$1) AS exists',
+		[username],
+	)
+	return exists.rows[0].exists
 }
