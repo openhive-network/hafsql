d2eacf0f2cb21c6cc4fe8aab5375c5c3685950ae
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:54:30 +0330

    Update setup_public_user.ts

--- src/app/setup/setup_public_user.ts
+++ src/app/setup/setup_public_user.ts
@@ -1,34 +1,32 @@
-import { pool } from '../helpers/database.ts'
+import { query } from '../helpers/database.ts'
 
 export const setupPublicUser = async () => {
-	using client = await pool.connect()
-
-	const role = await client.queryObject(
+	const role = await query(
 		'SELECT rolname FROM pg_catalog.pg_roles WHERE rolname = $1',
 		['hafsql_public'],
 	)
 	if (role.rowCount === 0) {
-		await client.queryObject(
+		await query(
 			"CREATE ROLE hafsql_public NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'hafsql_public';",
 		)
 	}
-	await client.queryObject('GRANT USAGE ON SCHEMA hafsql TO hafsql_public;')
-	await client.queryObject(
+	await query('GRANT USAGE ON SCHEMA hafsql TO hafsql_public;')
+	await query(
 		'GRANT SELECT ON ALL TABLES IN SCHEMA hafsql TO hafsql_public;',
 	)
-	await client.queryObject(
+	await query(
 		'GRANT ALL ON FUNCTION hafd._operation_to_jsonb TO hafsql_public;',
 	)
-	await client.queryObject(
+	await query(
 		'GRANT ALL ON FUNCTION hive.operation_id_to_block_num TO hafsql_public;',
 	)
-	await client.queryObject(
+	await query(
 		'GRANT ALL ON FUNCTION hive.operation_id_to_type_id TO hafsql_public;',
 	)
-	await client.queryObject(
+	await query(
 		"ALTER USER hafsql_public SET statement_timeout='45s';",
 	)
-	await client.queryObject(
+	await query(
 		'ALTER USER hafsql_public SET search_path TO hafsql, public;',
 	)
 	console.log('hafsql_public user setup done')
