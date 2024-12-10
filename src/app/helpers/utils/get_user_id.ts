d5c540506ed1eb9efae24270d74276cc6c062bbc
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2024-12-10 14:54:09 +0330

    Update get_user_id.ts

--- src/app/helpers/utils/get_user_id.ts
+++ src/app/helpers/utils/get_user_id.ts
@@ -1,39 +1,32 @@
-import { PoolClient, Transaction } from '../../../deps.ts'
-import { pool } from '../database.ts'
+import { customClient, query } from '../database.ts'
 import { clearUsername } from './validate_username.ts'
 
 let accountCache: Record<string, number> = {}
 
 const clearCache = () => {
-  accountCache = {}
+	accountCache = {}
 }
 // every 10min clear cache
 setInterval(() => clearCache(), 600000)
 
 // Caching ids for duration of the sync
-export const getUserId = async (username: string, trx?: Transaction) => {
-  username = clearUsername(username)
-  if (Object.hasOwn(accountCache, username)) {
-    return accountCache[username]
-  } else {
-    let client
-    if (trx) {
-      client = trx
-    } else {
-      client = await pool.connect()
-    }
-    const getId = await client.queryObject<{ id: number }>(
-      'SELECT a.id FROM hafd.accounts a WHERE a.name=$1',
-      [username],
-    )
-    if (!trx && client instanceof PoolClient) {
-      client.release()
-    }
-    if (getId.rows.length < 1) {
-      return null
-    }
-    const id = getId.rows[0].id
-    accountCache[username] = id
-    return id
-  }
+export const getUserId = async (
+	username: string,
+	client: customClient = { query },
+) => {
+	username = clearUsername(username)
+	if (Object.hasOwn(accountCache, username)) {
+		return accountCache[username]
+	} else {
+		const getId = await client.query<{ id: number }>(
+			'SELECT a.id FROM hafd.accounts a WHERE a.name=$1',
+			[username],
+		)
+		if (getId.rows.length < 1) {
+			return null
+		}
+		const id = getId.rows[0].id
+		accountCache[username] = id
+		return id
+	}
 }
