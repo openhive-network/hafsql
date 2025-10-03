74f275a2ccc144afb4adbb0baccb3b3acb997436
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-10-03 13:43:37 +0330

    fix block_num validation of API - block_num=0 invalid

--- src/api/helpers/validate_block_num.ts
+++ src/api/helpers/validate_block_num.ts
@@ -1,19 +1,19 @@
 import { Context } from '../../deps.ts'
 
 export const validateBlockNum = (
-  ctx: Context,
-  blockNum: string | null,
+	ctx: Context,
+	blockNum: string | null,
 ) => {
-  if (!blockNum) {
-    return badValueError(ctx)
-  }
-  const num = Number(blockNum)
-  if (isNaN(num) || num < 0 || num > 2147483647 || !Number.isInteger(num)) {
-    return badValueError(ctx)
-  }
-  return num
+	if (!blockNum) {
+		return badValueError(ctx)
+	}
+	const num = Number(blockNum)
+	if (isNaN(num) || num <= 0 || num > 2147483647 || !Number.isInteger(num)) {
+		return badValueError(ctx)
+	}
+	return num
 }
 
 const badValueError = (ctx: Context) => {
-  ctx.throw(400, 'Invalid value for block_num - Example: 5000000')
+	ctx.throw(400, 'Invalid value for block_num - Example: 5000000')
 }
