174b1b351e3dbd485eb3752bbbd12cf02b5012db
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-07 19:55:09 +0330

    Handle upgrading from older version

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -34,13 +34,40 @@ export const handleUpgrade = async () => {
 		await transaction(async (client) => {
 			for (const [name, _value] of Object.entries(operationTables)) {
 				// The tables should exists because setup already ran
-				await client.query(`DROP TABLE hafsql.operation_${name}_table CASCADE;`)
+				await client.query(
+					`DROP TABLE IF EXISTS hafsql.operation_${name}_table CASCADE;`,
+				)
 			}
-			await client.query(`DROP TABLE hafsql.accounts_table CASCADE;`)
+			await client.query(`DROP TABLE IF EXISTS hafsql.accounts_table CASCADE;`)
 			await client.query(
 				`UPDATE hafsql.sync_data SET last_block_num=0 WHERE table_name IN ('accounts', 'operations')`,
 			)
 		})
+		print('Upgrade script done ✅')
+		// Run setup again to recreate the tables
+		await setup()
+	}
+
+	// Upgrading from = 2.3.2
+	// Some tables were removed
+	if (semver.equals(semver.parse(oldVersion), semver.parse('2.3.2'))) {
+		print(
+			'Upgrading from version = 2.3.2 - some tables are removed in favor of reptracker and hafbe',
+		)
+		await query('DROP TABLE IF EXISTS hafsql.blacklists_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.blacklist_follows_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.mutes_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.mute_follows_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.reblogs_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.follows_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.community_roles_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.reputations_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.balances_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.balances_history_table CASCADE;')
+		await query('DROP TABLE IF EXISTS hafsql.total_balances_table CASCADE;')
+		await query(
+			'DROP TABLE IF EXISTS hafsql.pending_saving_withdraws_table CASCADE;',
+		)
 		print('Upgrade script done ✅')
 		// Run setup again to recreate the tables
 		await setup()
