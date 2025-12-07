ad61c54558c353c0fd49e4789c3f3797cbd2186c
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-07 22:23:17 +0330

    handle upgrade for operation tables as well

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -54,20 +54,34 @@ export const handleUpgrade = async () => {
 		print(
 			'Upgrading from version = 2.3.2 - some tables are removed in favor of reptracker and hafbe',
 		)
-		await query('DROP TABLE IF EXISTS hafsql.blacklists_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.blacklist_follows_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.mutes_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.mute_follows_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.reblogs_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.follows_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.community_roles_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.reputations_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.balances_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.balances_history_table CASCADE;')
-		await query('DROP TABLE IF EXISTS hafsql.total_balances_table CASCADE;')
-		await query(
-			'DROP TABLE IF EXISTS hafsql.pending_saving_withdraws_table CASCADE;',
-		)
+		await transaction(async (client) => {
+			for (const [name, _value] of Object.entries(operationTables)) {
+				await client.query(
+					`DROP TABLE IF EXISTS hafsql.operation_${name}_table CASCADE;`,
+				)
+			}
+			await client.query(
+				`UPDATE hafsql.sync_data SET last_block_num=0 WHERE table_name IN ('operations')`,
+			)
+			const query = client.query
+			await query('DROP TABLE IF EXISTS hafsql.blacklists_table CASCADE;')
+			await query(
+				'DROP TABLE IF EXISTS hafsql.blacklist_follows_table CASCADE;',
+			)
+			await query('DROP TABLE IF EXISTS hafsql.mutes_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.mute_follows_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.reblogs_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.follows_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.community_roles_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.reputations_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.balances_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.balances_history_table CASCADE;')
+			await query('DROP TABLE IF EXISTS hafsql.total_balances_table CASCADE;')
+			await query(
+				'DROP TABLE IF EXISTS hafsql.pending_saving_withdraws_table CASCADE;',
+			)
+		})
+
 		print('Upgrade script done ✅')
 		// Run setup again to recreate the tables
 		await setup()
