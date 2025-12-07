e40e6324ffd3e746b4d10aee4ba4375874ce6e01
Mahdi Yari <16903082+mahdiyari@users.noreply.github.com>
2025-12-07 22:46:12 +0330

    remove unused workers

--- src/app/helpers/createWorkers.ts
+++ src/app/helpers/createWorkers.ts
@@ -14,7 +14,7 @@ export const createWorkers = async () => {
 	// op_type_id id
 	if (
 		isSyncing.one === false &&
-		await doesIndexExist('hafsql_hive_operations_op_type_id_id') &&
+		(await doesIndexExist('hafsql_hive_operations_op_type_id_id')) &&
 		!(await getBlockRange('operations'))
 	) {
 		isSyncing.one = true
@@ -39,19 +39,10 @@ export const createWorkers = async () => {
 	// custom_json id
 	if (
 		isSyncing.two === false &&
-		await doesIndexExist('hafsql_id_opid_idx') &&
+		(await doesIndexExist('hafsql_id_opid_idx')) &&
 		!(await getBlockRange('operations'))
 	) {
 		isSyncing.two = true
-		if (
-			Deno.env.get('HAFSQL_REBLOGS') !== 'false' &&
-			Deno.env.get('HAFSQL_COMMENTS') !== 'false'
-		) {
-			// reblogs
-			createWorker('../sync/reblogs.ts').postMessage('start')
-			print('[Main] Starting reblogs worker 👷')
-		}
-
 		if (Deno.env.get('HAFSQL_RC_DELEGATIONS') !== 'false') {
 			// rc_delegations
 			createWorker('../sync/rc_delegations.ts').postMessage('start')
@@ -62,7 +53,7 @@ export const createWorkers = async () => {
 	// author permlink
 	if (
 		isSyncing.three === false &&
-		await doesIndexExist('hafsql_author_permlink_idx') &&
+		(await doesIndexExist('hafsql_author_permlink_idx')) &&
 		!(await getBlockRange('operations'))
 	) {
 		isSyncing.three = true
